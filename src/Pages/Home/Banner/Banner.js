7b1f4e24fa7cbada48f2d6466c70fc5a21811b4a
Toha Islam <islamtoha710@gmail.com>
2022-11-24 20:51:19 +0600

    added image in banner section

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className="carousel w-full h-screen">
+        <div className="carousel w-full md:h-screen lg:h-screen">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src="https://media.istockphoto.com/id/1212526330/photo/bohemian-living-room-interior-3d-render.jpg?s=612x612&w=0&k=20&c=iIQ5wqa4NYpLn0YJvT_NAzMilwTFkTlprwbXAasOn9s=" className="w-full" alt='' />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -11,21 +11,21 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-                <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                <img src="https://media.istockphoto.com/id/1328744010/photo/modern-living-room-interior-3d-render.jpg?s=612x612&w=0&k=20&c=FedJ6adq_S4WvCBWdif-bQq6SXUqge7JWsTE-MWzZ2g=" className="w-full" alt='' />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-                <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                <img src="https://media.istockphoto.com/id/1293762741/photo/modern-living-room-interior-3d-render.jpg?b=1&s=170667a&w=0&k=20&c=Ko289acalSrEKwYI5sb20u2WQa7KS9L-_0Ug4u2iEmc=" className="w-full" alt='' />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
-                <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                <img src="https://media.istockphoto.com/id/1333060261/photo/modern-living-room-interior-design.jpg?s=170667a&w=0&k=20&c=apMNWk3bGCpr4uhdNFjN1f-RxnDAO-0V4uDCW5C4bLA=" className="w-full" alt='' />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide3" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
